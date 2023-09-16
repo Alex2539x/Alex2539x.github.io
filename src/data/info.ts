1001e23e2ee0d1ccf8dc50a7fce7eefff46488e1
Alex2539x <alex2539x@gmail.com>
2023-09-16 16:55:33 -0400

    Add updated information, update logos

--- src/data/info.ts
+++ src/data/info.ts
@@ -78,11 +78,11 @@ export const info = {
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    facebook: "https://www.facebook.com/profile.php?id=100085804996924",
+    twitter: "https://twitter.com/Bugle61917458",
+    github: "https://github.com/Alex2539x",
+    email: "alex2539x@gmail.com",
+    linkedin: "https://www.linkedin.com/in/alex-ho-057819243/",
   },
 
   projects: [
